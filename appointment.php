d2a760cbd02c374e525385a0f9fc5a260afb2d78
white-veld <tyutyunin2004@mail.ru>
2025-05-23 04:16:36 +0800

    Update appointment

--- appointment.php
+++ appointment.php
@@ -6,12 +6,17 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Запись. Фотограф Новосибирск Алина Липасова</title>
     <link rel="stylesheet" href="/css/main.css">
+    <link href="https://calendar.google.com/calendar/scheduling-button-script.css" rel="stylesheet">
+    <script src="https://calendar.google.com/calendar/scheduling-button-script.js" async></script>
 </head>
 <body>
     <?php require_once "blocks/header.php"; ?>
-    <!-- Google Calendar Appointment Scheduling begin -->
-    <iframe src="https://calendar.google.com/calendar/appointments/schedules/AcZssZ1Ely759u-BV3h3GdXzIxjMWUgIpRUB0vsDdfX7Og_hYqSg-qH66XxRm1wutm8dGYkAMLXEMDpk?gv=true" style="border: 0" width="100%" height="600" frameborder="0"></iframe>
-    <!-- end Google Calendar Appointment Scheduling -->
+    <div id="rules-appointment">
+        <p>Правила записи</p>
+    </div>
+    <div id="appointment">
+        <script src="/js/calendar.js"></script>
+    </div>
     <?php require_once "blocks/contacts.php"; ?>
 </body>
 </html>
---
+++ js/calendar.js
@@ -0,0 +1,11 @@
+(function() {
+  var target = document.currentScript;
+  window.addEventListener('load', function() {
+    calendar.schedulingButton.load({
+      url: 'https://calendar.google.com/calendar/appointments/schedules/AcZssZ0j9b7jn9c2bH-4r33fWepBQFaL0yf81lcQzj6ae6yvuxwC4r8j6YfDNd3qrLvEP-hXusXX-n9Q?gv=true',
+      color: '#ffffff',
+      label: "\u0417\u0430\u043F\u0438\u0441\u0430\u0442\u044C\u0441\u044F \u043D\u0430 \u0444\u043E\u0442\u043E\u0441\u0435\u0441\u0441\u0438\u044E",
+      target,
+    });
+  });
+})();
